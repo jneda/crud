3be2f4df02ec89037c6065d66c3d8b6d20d6c970
jdane <julien.dane@gmail.com>
2022-12-20 15:22:56 +0100

    feat: success alert template

---
+++ success.php
@@ -0,0 +1,6 @@
+<section class="container mt-3">
+  <div class="row col-md-6 offset-md-3 alert alert-success">
+    <h1>Bravo <?= $login ?></h1>
+    <p>L'opération est un succès !🥳</p>
+  </div>
+</section>
